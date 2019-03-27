e554619657a4c91d5f6212befc0f6021d5eb6c11
Mehmet Eray Sürmeli <39830419+MehmetEray@users.noreply.github.com>
2019-03-27 21:04:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 // Session Storage
 // Butonları seçmek
-// const add = document.querySelector("#add");
-// const del = document.querySelector("#delete");
-// const clear = document.querySelector("#clear");
+const add = document.querySelector("#add");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
 
-// // Inputlar
+// Inputlar
 
-// const addkey = document.querySelector("#addkey");
-// const addvalue = document.querySelector("#addvalue");
-// const deletekey = document.querySelector("#deletekey");
+const addkey = document.querySelector("#addkey");
+const addvalue = document.querySelector("#addvalue");
+const deletekey = document.querySelector("#deletekey");
 
-// add.addEventListener("click",addItems);
-// del.addEventListener("click",deleteItems);
-// clear.addEventListener("click",clearItems);
+add.addEventListener("click",addItems);
+del.addEventListener("click",deleteItems);
+clear.addEventListener("click",clearItems);
 
-// function addItems(e){
-//     sessionStorage.setItem(addkey.value,addvalue.value);
-// }
-// function deleteItems(e){
-//     sessionStorage.removeItem(deletekey.value);
-// }
-// function clearItems(e){
-//     sessionStorage.clear();
-// }
+function addItems(e){
+    sessionStorage.setItem(addkey.value,addvalue.value);
+}
+function deleteItems(e){
+    sessionStorage.removeItem(deletekey.value);
+}
+function clearItems(e){
+    sessionStorage.clear();
+}
 
 // Local Storage
 
-// SetItem
+SetItem
 
-// localStorage.setItem("hareket","burpee");
-// localStorage.setItem("tekrar",50);
-// GetItem
+localStorage.setItem("hareket","burpee");
+localStorage.setItem("tekrar",50);
+GetItem
 
-// const value = localStorage.getItem("tekrar");
-// console.log(value);
-// console.log(typeof value);
+const value = localStorage.getItem("tekrar");
+console.log(value);
+console.log(typeof value);
 // Clear local storage
 
-// console.log(localStorage.getItem("sport"));
-// if(localStorage.getItem("hareket") != null){
-//     console.log("Sorguladığınız veri bulunuyor");
-// }
-// else{
-//     console.log("Sorguladığınız veri bulunamıyor");
-// }
+console.log(localStorage.getItem("sport"));
+if(localStorage.getItem("hareket") != null){
+    console.log("Sorguladığınız veri bulunuyor");
+}
+else{
+    console.log("Sorguladığınız veri bulunamıyor");
+}
 
 // Local storage array yazma
 
-// const todos = ["one","two","three"];
-// localStorage.setItem("todos",JSON.stringify(todos));
-// const value = JSON.parse(localStorage.getItem("todos"));
-// console.log(value);
+const todos = ["one","two","three"];
+localStorage.setItem("todos",JSON.stringify(todos));
+const value = JSON.parse(localStorage.getItem("todos"));
+console.log(value);
 
 const form = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo");
